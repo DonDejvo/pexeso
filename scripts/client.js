42aac18b3acc4151f0db986750c6717d95c98087
r8w9 <28980824+r8w9a5k@users.noreply.github.com>
2021-03-30 13:33:10 +0200

    android bug fix
    touchstart and click opens more cards :X

--- scripts/client.js
+++ scripts/client.js
@@ -109,20 +109,6 @@ function createBoard() {
 	id("board").innerHTML = html;
 }
 
-function preventZoom(e) {
-  var t2 = e.timeStamp;
-  var t1 = e.currentTarget.dataset.lastTouch || t2;
-  var dt = t2 - t1;
-  var fingers = e.touches.length;
-  e.currentTarget.dataset.lastTouch = t2;
-
-  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-  e.preventDefault();
-  e.target.click();
-  alert("preventZoom");
-}
-
 function createEvents() {
 	addEventListener('click', handleClick);
 	addEventListener('touchstart', handleClick);
@@ -135,7 +121,7 @@ function createEvents() {
     		}
   	}, {passive: false});
 	
-	addEventListener('touchstart', preventZoom); 
+	//addEventListener('touchstart', preventZoom); 
 }
 
 function handleClick(e) {
